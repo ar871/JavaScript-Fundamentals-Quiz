427f77c965ee34341dbfbff2e208261bb6eb53a6
ar871 <arehman871@hotmail.com>
2023-01-20 22:34:49 +0000

    added questions and pseudo code to the js files

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,18 @@
+/* a click to start game button.
+once clicked first question shows up.
+if question is answer correct use correct sound effect  and give user 1 point and move on to next question.
+if answer is incorrect use inccorect sound take away 10 seconds from timer and remain on same question.
+if timer reachers 0 end game and allow user to enter initals for scoreboard.
+go back button availabe to try again or a clear scporeboard button 
+
+
+
+pottential code started 
+let numebrOfQuestion = arrayOfQuestions.length-------- so basically 5 question
+let points = 0
+let pointsToAdd = 1
+let timer = 60 
+let timePenalty = -10
+
+
+ */
