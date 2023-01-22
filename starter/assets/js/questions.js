a95301263bf82522c97b49c39c59131584a31b1d
ar871 <arehman871@hotmail.com>
2023-01-22 22:01:30 +0000

    added array of questions with answer

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -33,3 +33,39 @@ each question should be an array with nested array being the answers
 
 ]
 */ 
+let listOfQuestions = [
+["which characters combined represents srict equality?",
+["==",
+"!==",
+"===",
+"!="],2
+],
+["Using a ______ element in html we are able to link our JavaScript to HTML",
+["<code>",
+"<script>",
+"<js>",
+"<connect>"],1
+],
+["Where is the correct place to insert a JavaScript?",
+["between the <head> elements",
+"between the <body> elements",
+"between the <div> elements",
+"between the <h1> elements"],1
+],
+["How can you add a comment in a JavaScript?",
+["//like this",
+"**like this",
+"<!--like this-->",
+"\\like this"],0
+],
+["How do you declare a JavaScript variable?",
+["v variable;",
+"variable =;",
+"variable;",
+"var variable ;"],3
+],
+
+
+
+
+]
