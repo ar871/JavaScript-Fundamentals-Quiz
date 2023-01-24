e5524d6f19328347ea925ed6d600434bae37048f
ar871 <arehman871@hotmail.com>
2023-01-24 13:49:12 +0000

    questions and timer now work,gives final score

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,38 +1,4 @@
-/*
-;et arrayOfQuestions = [
 
-
-5 questions for javascript fundementals
-1)which characters combined represents srict equality?
-    a) ==
-    b) !==
-    c) === (this one)
-    d) !=
-2) Using a ______ element in html we are able to link our JavaScript to HTML
-    a) <code>
-    b) <script> (this one)
-    c) <js>
-    d) <connect>
-3) Where is the correct place to insert a JavaScript?
-    a) between the <head> elements
-    b) between the <body> elements(this one)
-    c) between the <div> elements
-    d) between the <h1> elements
-4) How can you add a comment in a JavaScript?
-    a) //like this (this one)
-    b) **like this 
-    c) <!--like this-->
-    d)\\like this
-5) How do you declare a JavaScript variable?
-    a) v variable;
-    b) variable =;
-    c) variable;
-    d) var variable ; (this one)
-
-each question should be an array with nested array being the answers
-
-]
-*/ 
 let listOfQuestions = [
 ["which characters combined represents srict equality?",
 ["==",
